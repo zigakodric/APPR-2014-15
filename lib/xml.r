ab46da512480689bff84320f7597989db0235706
zigakodric <kodric@hotmail.com>
2015-01-10 12:44:14 +0100

    Popravil napako

--- lib/xml.r
+++ lib/xml.r
@@ -1,40 +1,36 @@
-# Uvoz iz spletne strani
+# Uvoz s spletne strani
 library(XML)
-
 # Vrne vektor nizov z odstranjenimi začetnimi in končnimi "prazninami" (whitespace)
 # iz vozlišč, ki ustrezajo podani poti.
 stripByPath <- function(x, path) {
   unlist(xpathApply(x, path,
-                    function(y) gsub("^\\s*(.*?)Â?\\s*$", "\\1", xmlValue(y[[1]]))))
+                    function(y) gsub("^\\s*(.*?)\\s*$", "\\1",
+                                     gsub("[^[:alnum:] (),]", "",
+                                          xmlValue(y)))))
 }
-
-uvozi.tabelo <- function() {
-  url.podatki <- "http://epp.eurostat.ec.europa.eu/tgm/table.do?tab=table&init=1&language=en&pcode=tsdsc410&plugin=1"
-  doc.podatki <- htmlTreeParse(url.podatki, useInternalNodes=TRUE)
-  
+uvozi.evropa <- function() {
+  url.rodnostEU <- "http://ec.europa.eu/eurostat/tgm/table.do?tab=table&init=1&language=en&pcode=tsdsc410&plugin=1"
+  con <- url(url.rodnostEU)
+  doc.rodnostEU <- htmlTreeParse(readLines(con), useInternalNodes=TRUE)
+  close(con)
   # Poiščemo vse tabele v dokumentu
-  tabele <- getNodeSet(doc.podatki, "//table")
-  
+  tabele <- getNodeSet(doc.rodnostEU, "//table")
   # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
   # trenutnim vozliščem
   vrstice <- getNodeSet(tabele[[6]], ".//tr")
-  
   # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
   # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
   seznam <- lapply(vrstice, stripByPath, "./td/div")
-  
   # Iz seznama vrstic naredimo matriko
   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
-  
   # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
   colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[4]], ".//th/div"))
-  rownames(matrika) <- gsub("\n", " ", stripByPath(tabele[[5]], ".//th"))
-  
+  imena <- gsub("\n", " ", stripByPath(tabele[[5]], ".//th"))
   # Podatke iz matrike spravimo v razpredelnico
   return(
-    data.frame(apply(gsub("[,:]", "", matrika),
-                     2, as.numeric))
+    data.frame(apply(gsub("[^0-9]", "", matrika),
+                     2, as.numeric), row.names = imena)
   )
 }
 
-TabelaXML<- uvozi.tabelo()
+evropa <- uvozi.evropa()
