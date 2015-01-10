a0830d7d8b555f35329aabf360e9a31d2ab95dbb
zigakodric <kodric@hotmail.com>
2015-01-10 23:03:30 +0100

    Posodobil

--- projekt.r
+++ projekt.r
@@ -1,10 +1,11 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("uvoz/uvoz.r")
-
-# 3. faza: Analiza in vizualizacija podatkov
-source("vizualizacija/vizualizacija.r")
-
-# 4. faza: Napredna analiza podatkov
-source("analiza/analiza.r")
-
-cat("Končano.\n")
+source("uvoz/uvoz.r",encoding="UTF-8")
+source("slike/grafi.R",encoding="UTF-8")
+# # 3. faza: Analiza in vizualizacija podatkov
+# source("vizualizacija/vizualizacija.r")
+#
+source("vizualizacija/vizualizacija.r", encoding="UTF-8")
+# # 4. faza: Napredna analiza podatkov
+# source("analiza/analiza.r")
+#
+# cat("Končano.\n")
