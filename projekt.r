a3d67f48ab80d6ff144db80cee54c421c7b5db05
zigakodric <kodric@hotmail.com>
2015-09-21 18:25:34 +0200

    Dokončal projekt

--- projekt.r
+++ projekt.r
@@ -1,12 +1,13 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r",encoding="UTF-8")
-source("lib/xml.r")
-source("slike/graf.r",encoding="UTF-8")
-# # 3. faza: Analiza in vizualizacija podatkov
-# source("vizualizacija/vizualizacija.r")
-#
+source("lib/xml.r", encoding="UTF-8")
+source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+source("slike/graf.r", encoding="UTF-8")
+
+# 3. faza: Analiza in vizualizacija podatkov
 source("vizualizacija/vizualizacija.r", encoding="UTF-8")
-# # 4. faza: Napredna analiza podatkov
-# source("analiza/analiza.r")
-#
-# cat("Končano.\n")
+
+# 4. faza: Napredna analiza podatkov
+source("analiza/analizaS.r", encoding="UTF-8")
+
+cat("Končano.\n")
