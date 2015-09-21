f11382c31a99e8f19960a1f8aeae18e93ca82511
zigakodric <kodric@hotmail.com>
2015-09-21 18:23:15 +0200

    Dodal graf

--- slike/graf.r
+++ slike/graf.r
@@ -1,15 +1,14 @@
 # Graf
-# Funkcija, ki uvozi podatke za graf in ukaz, ki graf nariše
-pdf("slike/grafi.pdf", paper="a4r")
-uvoziPodatkeGraf <- function() {
-  return(read.table("podatki/grafzda.csv", sep = ",", as.is = TRUE,
-                    
-                    col.names = c("x","x", "x","x","x"),
-                    fileEncoding = "Windows-1250"))
-}
-
-TabelaGraf <- uvoziPodatkeGraf()
+# Graf ZDA
+pdf("slike/grafzda.pdf", width=6, height=4)
 Leto <- TabelaGraf[, "x.2"]
 Št_študentovZDA <- TabelaGraf[, "x.4"]
 plot(Leto, Št_študentovZDA, xlab = "Leto", ylab = "Število študentov", type = "l", main = "Število študentov v ZDA", cex = 0.5)
+dev.off()
+
+#Graf Kitajska
+pdf("slike/grafKit.pdf", width=6, height=4)
+LetoKit <- TabelaGrafKit[, "x.2"]
+Št_študentovKit <- TabelaGrafKit[, "x.4"]
+plot(LetoKit, Št_študentovKit, xlab = "Leto", ylab = "Število študentov", type = "l", main = "Število študentov na Kitajskem", cex = 0.5)
 dev.off()
