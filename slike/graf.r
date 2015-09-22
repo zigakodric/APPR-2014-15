7c7d827b116874fee941b0212b139d35dce17190
zigakodric <kodric@hotmail.com>
2015-09-22 10:31:21 +0200

    Popravil napis na grafu

--- slike/graf.r
+++ slike/graf.r
@@ -10,5 +10,5 @@ dev.off()
 pdf("slike/grafKit.pdf", width=6, height=4)
 LetoKit <- TabelaGrafKit[, "x.2"]
 Št_študentovKit <- TabelaGrafKit[, "x.4"]
-plot(LetoKit, Št_študentovKit, xlab = "Leto", ylab = "Število študentov", type = "l", main = "Število študentov na Kitajskem", cex = 0.5)
+plot(LetoKit, Št_študentovKit, xlab = "Leto", ylab = "Število študento na 100000 prebivalcev", type = "l", main = "Število študentov na Kitajskem", cex = 0.5)
 dev.off()
